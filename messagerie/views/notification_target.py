38d600fd508e90504f493fa9492fed880956eef6
Arnold Louis <arnoldlouis237@gmail.com>
2026-01-08 15:36:35 +0100

    feat: ajout de l'action mark_as_read permettant de gerer l'accusé de reception du message

--- messagerie/views/notification_target.py
+++ messagerie/views/notification_target.py
@@ -1,3 +1,6 @@
+from django.utils import timezone
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from messagerie.models import NotificationTarget
@@ -5,5 +8,27 @@ from messagerie.serializers import NotificationTargetSerializer
 
 
 class NotificationTargetViewSet(ReadOnlyModelViewSet):
-    queryset = NotificationTarget.objects.select_related('eleve', 'notification')
+    """
+    Consultation des notifications envoyées
+    et accusé de lecture interne.
+    """
+
+    queryset = NotificationTarget.objects.select_related(
+        'eleve', 'notification'
+    )
     serializer_class = NotificationTargetSerializer
+
+    @action(detail=True, methods=['post'])
+    def mark_as_read(self, request, pk=None):
+        """
+        Marque la notification comme lue (accusé interne).
+        """
+        target = self.get_object()
+
+        # Idempotence
+        if target.statut != 'lu':
+            target.statut = 'lu'
+            target.date_lecture = timezone.now()
+            target.save()
+
+        return Response({'status': 'ok'})
