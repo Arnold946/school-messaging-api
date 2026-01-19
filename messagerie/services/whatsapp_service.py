41dea0631dfe3a1d23548616e2ddcb64809b01f1
Arnold Louis <arnoldlouis237@gmail.com>
2026-01-19 08:02:24 +0100

    feat: rendu fonctionnel du service whatsapp

--- messagerie/services/whatsapp_service.py
+++ messagerie/services/whatsapp_service.py
@@ -1,7 +1,25 @@
+from django.conf import settings
+from twilio.rest import Client
+import logging
+
+logger = logging.getLogger(__name__)
+
 class WhatsAppService:
     @staticmethod
-    def send(phone_number: str, message: str):
-        """
-        Envoi WhatsApp
-        """
-        print(f"[WHATSAPP] → {phone_number} : {message}")
+    def send(phone_number: str, message: str) -> str:
+        client = Client(
+            settings.TWILIO_ACCOUNT_SID,
+            settings.TWILIO_AUTH_TOKEN
+        )
+
+        try:
+            msg = client.messages.create(
+                body=message,
+                from_=settings.TWILIO_WHATSAPP_SANDBOX,
+                to=f"whatsapp:{phone_number}"
+            )
+            logger.info(f"WhatsApp envoyé | SID={msg.sid}")
+            return msg.sid
+        except Exception as e:
+            logger.error(f"Erreur WhatsApp Twilio : {e}")
+            raise
